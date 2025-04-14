1eeca01022cda722ffd82df0ddc8c1097fd39d5a
hibby <hibaqku7@gmail.com>
2025-04-14 23:44:22 +0300

    Added submit button to ExpenseForm.jsx

--- components/ExpenseForm.jsx
+++ components/ExpenseForm.jsx
@@ -19,13 +19,16 @@ const ExpenseForm= ({ onAddExpense}) => {
         onAddExpense(newExpense);
         setForm({ name:'', Description:'', Category: '', Date:''});
     };
+    return (
+        <form onSubmit={handleSubmit}>
+            <input name= "name" value={form.name} onChange={handleChange} placeholder="Expense Name" required/>
+            <input name= "description" value={form.Description} onChange={handleChange} placeholder="Description" required/>
+            <input name= "category" value={form.Category} onChange={handleChange} placeholder="Category" required/>
+            <input name= "amount" value={form.Amount} onChange={handleChange} type= "number" placeholder="Amount"/>
+            <input name= "date" value={form.Date} onChange={handleChange} type="date" placeholder="Date" />
+            <button type="submit">Submit</button>
+        </form>
+    );
     
-}
-
-
-
-
-
-
-
+};
 export default ExpenseForm;
