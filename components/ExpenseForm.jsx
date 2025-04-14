9ee1d9566b7559f5a6e0434fd756561133cef731
hibby <hibaqku7@gmail.com>
2025-04-14 22:53:20 +0300

    delete everything in app.jsx, main.jsx, create project files to populate

---
+++ components/ExpenseForm.jsx
@@ -0,0 +1,17 @@
+import React, {useState} from "react";
+const ExpenseForm= ({ onAddExpense}) => {
+    const [form,setForm]=useState({
+        name: '',
+        Description: '',
+        Category: '',
+        Amount: '',
+        Date: ''
+    });
+}
+
+
+
+
+
+
+export default ExpenseForm;
